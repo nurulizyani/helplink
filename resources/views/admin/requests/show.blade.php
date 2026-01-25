eba29b20ad2a9e0cc85967a33e2bb42598569fb9
NURUL IZYANI <nrl.izyani03@gmail.com>
2026-01-25 15:27:27 +0800

    Fix reject and approve modal

--- resources/views/admin/requests/show.blade.php
+++ resources/views/admin/requests/show.blade.php
@@ -231,5 +231,68 @@
         </div>
     @endif
 </div>
+<div class="modal fade" id="approveModal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <form method="POST"
+              action="{{ route('admin.requests.approve', $request->id) }}"
+              class="modal-content rounded-4">
+            @csrf
+
+            <div class="modal-header">
+                <h5 class="modal-title">Approve Request</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <div class="modal-body">
+                Are you sure you want to approve this request?
+            </div>
+
+            <div class="modal-footer">
+                <button type="button"
+                        class="btn btn-light"
+                        data-bs-dismiss="modal">
+                    Cancel
+                </button>
+
+                <button type="submit"
+                        class="btn btn-success">
+                    Yes, Approve
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<div class="modal fade" id="rejectModal" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+        <form method="POST"
+              action="{{ route('admin.requests.reject', $request->id) }}"
+              class="modal-content rounded-4">
+            @csrf
+
+            <div class="modal-header">
+                <h5 class="modal-title">Reject Request</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+
+            <div class="modal-body">
+                Are you sure you want to reject this request?
+            </div>
+
+            <div class="modal-footer">
+                <button type="button"
+                        class="btn btn-light"
+                        data-bs-dismiss="modal">
+                    Cancel
+                </button>
+
+                <button type="submit"
+                        class="btn btn-danger">
+                    Yes, Reject
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
 
 @endsection
